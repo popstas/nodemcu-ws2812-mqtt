4dc77bf6cfc5eae0017616b00082b1c2f0ec8f01
Stanislav Popov <popstas@gmail.com>
2017-06-05 03:13:46 +0500

    fix: round power meter output

--- ws2812-power.lua
+++ ws2812-power.lua
@@ -7,6 +7,7 @@ end
 return function(buffer)
     local psu_max_ma = 40000
     local power_ma = get_power(buffer)
-    local power_percent = power_ma * 100 / psu_max_ma
-    return { a = power_ma / 1000, power = power_ma * 5 / 1000, percent =  power_percent }
+    local power_percent = math.floor(power_ma * 100 / psu_max_ma)
+    local power = math.floor(power_ma * 5 / 1000)
+    return { a = power_ma / 1000, power = power, percent = power_percent }
 end
