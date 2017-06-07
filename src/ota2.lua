7b8f9f1951ce9f73360041e5414f8368ea4b5da4
Stanislav Popov <popstas@gmail.com>
2017-06-07 21:35:49 +0500

    fix: ota debug

--- src/ota2.lua
+++ src/ota2.lua
@@ -4,29 +4,34 @@ telnet_srv = net.createServer(net.TCP, 180)
 local function ota2_start()
     local cmd, args, body_started, f, valid
     local invalid = 0
+    local debug_level = 1
+
     telnet_srv:listen(2323, function(socket)
         local fifo = {} local fifo_drained = true
         local function sender(c)
             if #fifo > 0 then c:send(table.remove(fifo, 1)) else fifo_drained = true end
         end
 
-        local function debug(str)
-            --print(str)
+        local function debug(str, level)
+            if debug_level >= level then print(str) end
         end
 
         local function s_input(c, str)
             if cmd then
+
                 if body_started then
+                    -- stage 3.5
                     if str == '#!endbody' then
-                        --debug('end body')
+                        -- stage 4
+                        debug('end body', 2)
                         if cmd == 'upload' then
                             f:close()
-                            --debug('file closed')
+                            debug('file closed', 2)
                             if args.length then
                                 local stat = file.stat('_ota_temp')
                                 valid = stat.size == tonumber(args.length)
                                 if not valid then
-                                    --debug('invalid upload, expected '..args.length..' bytes, received '..stat.size)
+                                    debug('invalid upload, expected '..args.length..' bytes, received '..stat.size, 0)
                                     invalid = invalid + 1
                                 end
                                 stat = nil
@@ -34,42 +39,46 @@ local function ota2_start()
                             if valid then
                                 if file.exists(args.filename) then file.remove(args.filename) end
                                 file.rename('_ota_temp', args.filename)
-                                --debug('file renamed')
+                                debug('file renamed', 2)
                             end
-                            print('total invalid:', invalid)
+                            debug('total invalid:'..invalid, 1)
                         end
                         body_started = false
                         cmd = nil
                         args = nil
                         f = nil
                         valid = nil
-                        print('memory:', node.heap())
+                        debug('memory: '..node.heap(), 1)
                     else
+                        -- stage 3 process
                         if cmd == 'upload' then
                             --print(str)
                             f.write(str)
                         end
                     end
                 elseif str == '#!body' then
+                    -- stage 3 start
                     body_started = true
                     if cmd == 'upload' and args.filename then
                         valid = true
                         file.close()
                         f = file.open('_ota_temp', 'w')
-                        --debug('start upload')
+                        debug('start upload', 2)
                     end
                 else
+                    -- stage 2
                     local name, value = str:match('#!arg:(.*)=(.*)')
                     if name then
-                        --debug('received arg:', name, value)
+                        debug('received arg: '..name..' = '..value, 2)
                         args[name] = value
                     end 
                     
                 end
             else
+                -- stage 1
                 cmd = str:match('^#!cmd:(.*)')
                 if cmd then
-                    print('received cmd:', cmd)
+                    debug('received cmd: '..cmd, 1)
                     args = {}
                 end
             end
